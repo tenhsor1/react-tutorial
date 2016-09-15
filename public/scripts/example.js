63a121257d4cb7b3f12245b1d8c3551aba279ded
Rick <tenhsor@mac-rick.local>
2016-09-14 20:04:26 -0500

    Added comment box component

---
+++ public/scripts/example.js
@@ -0,0 +1,15 @@
+var CommentBox = React.createClass({
+    render: function(){
+        return (
+            <div className="commentBox">
+                Hello, World! I am a CommentBox.
+            </div>
+        );
+    }
+});
+
+
+ReactDOM.render(
+    <CommentBox />,
+    document.getElementById('content')
+);
