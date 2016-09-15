359b3dd32a3d8c049fcce27de7be0e6684294e04
Rick <tenhsor@mac-rick.local>
2016-09-14 20:08:40 -0500

    Created comment list and form components

--- public/scripts/example.js
+++ public/scripts/example.js
@@ -2,12 +2,33 @@ var CommentBox = React.createClass({
     render: function(){
         return (
             <div className="commentBox">
-                Hello, World! I am a CommentBox.
+                <h1>Comments</h1>
+                <CommentList />
+                <CommentForm />
             </div>
         );
     }
 });
 
+var CommentList = React.createClass({
+    render: function(){
+        return (
+            <div className="commentList">
+                Hello, World! I am a CommentList
+            </div>
+        );
+    }
+});
+
+var CommentForm = React.createClass({
+    render: function(){
+        return (
+            <div className="commentForm">
+                Hello, World! I am a CommentForm.
+            </div>
+        );
+    }
+});
 
 ReactDOM.render(
     <CommentBox />,
